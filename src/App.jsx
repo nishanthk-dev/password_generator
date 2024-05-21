2546d5beda009f131b31adb48e9a41257d6bfd0e
nishathk200 <nishanthk200@gmail.com>
2024-05-21 16:22:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,183 @@
+import { useState } from "react";
+import "./App.css";
+
+const numbers = "0123456789";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const specialCharacters = "!^+%&/()?_#{$[]}|;:><.*-@";
+const COPY_FAIL = "Password successfully copied to clipboard";
+const COPY_SUCCESS = "Password successfully copied to clipboard";
+
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(16);
+  const [includeUpperCase, setIncludeUpperCase] = useState(true);
+  const [includeLowerCase, setIncludeLowerCase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [includeSymbols, setIncludeSymbols] = useState(true);
+  const [lastFivePasswords, setLastFivePasswords] = useState([]);
+
+  const handleGeneratePassword = () => {
+    if (
+      !includeUpperCase &&
+      !includeLowerCase &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      notify(
+        "To generate password you must select at least one checkbox",
+        true
+      );
+    } else {
+      let characterList = "";
+      if (includeNumbers) {
+        characterList = characterList + numbers;
+      }
+      if (includeUpperCase) {
+        characterList = characterList + upperCaseLetters;
+      }
+      if (includeLowerCase) {
+        characterList = characterList + lowerCaseLetters;
+      }
+      if (includeSymbols) {
+        characterList = characterList + specialCharacters;
+      }
+      const newPassword = createPassword(characterList);
+      setPassword(newPassword);
+      setLastFivePasswords((prevPasswords) => {
+        const updatedPasswords = [...prevPasswords.slice(-4), newPassword];
+        return updatedPasswords;
+      });
+      notify("Password is generated successfully", false);
+    }
+  };
+
+  const createPassword = (characterList) => {
+    let password = "";
+    const characterListLength = characterList.length;
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength);
+      password = password + characterList.charAt(characterIndex);
+    }
+    return password;
+  };
+
+  const copyToClipboard = (password) => {
+    navigator.clipboard.writeText(password);
+  };
+
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      alert(message);
+    }
+  };
+
+  const handleCopyPassword = () => {
+    if (password === "") {
+      notify(COPY_FAIL);
+    } else {
+      copyToClipboard(password);
+      notify(COPY_SUCCESS);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">Password Generator</h2>
+          <div className="generator__password">
+            <h3>{password}</h3>
+            <button className="copy__btn" onClick={handleCopyPassword}>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                height="1.5em"
+                width="1.5em"
+              >
+                <path d="M4 1.5H3a2 2 0 00-2 2V14a2 2 0 002 2h10a2 2 0 002-2V3.5a2 2 0 00-2-2h-1v1h1a1 1 0 011 1V14a1 1 0 01-1 1H3a1 1 0 01-1-1V3.5a1 1 0 011-1h1v-1z" />
+                <path d="M9.5 1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-3a.5.5 0 01-.5-.5v-1a.5.5 0 01.5-.5h3zm-3-1A1.5 1.5 0 005 1.5v1A1.5 1.5 0 006.5 4h3A1.5 1.5 0 0011 2.5v-1A1.5 1.5 0 009.5 0h-3z" />
+              </svg>
+            </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">
+              Password length: {passwordLength}
+            </label>
+            <input
+              className="pw"
+              defaultValue={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
+              type="range"
+              id="password-stregth"
+              name="password-strength"
+              max="26"
+              min="8"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Add Uppercase Letters</label>
+            <input
+              checked={includeUpperCase}
+              onChange={(e) => setIncludeUpperCase(e.target.checked)}
+              type="checkbox"
+              id="uppercase-letters"
+              name="uppercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Add Lowercase Letters</label>
+            <input
+              checked={includeLowerCase}
+              onChange={(e) => setIncludeLowerCase(e.target.checked)}
+              type="checkbox"
+              id="lowercase-letters"
+              name="lowercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+            />
+          </div>
+          <button onClick={handleGeneratePassword} className="generator__btn">
+            Generate Password
+          </button>
+        </div>
+      </div>
+      <div className="prev-passwords">
+        <h3>Previous Passwords:</h3>
+        <ul className="">
+          {lastFivePasswords.map((password, index) => (
+            <li
+              className="last-password"
+              key={index}
+              onClick={() => {
+                navigator.clipboard.writeText(password);
+              }}
+            >
+              {password}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
